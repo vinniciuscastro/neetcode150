58257a3390761bd05823846105262fcd54a9e564
Vinnicius Castro <90991299+vinniciuscastro@users.noreply.github.com>
2025-06-11 14:31:37 -0600

    cycle-I

--- Linked List/cycle2.py
+++ Linked List/cycle2.py
@@ -6,25 +6,4 @@ class ListNode:
     """
     Resolution: Slow and Fast Pointer Technique (Floyd's Cycle Detection Algorithm)
     This algorithm uses two pointers, one moving at twice the speed of the other."""
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next  
-    def detectCycle(head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return None
-        
-        slow = head
-        fast = head
-        
-        # Detect cycle using Floyd's Tortoise and Hare algorithm
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            
-            if slow == fast:
-                # Cycle detected, find the entry point
-                entry = head
-                while entry != slow:
-                    entry = entry.next
-                    slow = slow.next
-                return entry
+   
