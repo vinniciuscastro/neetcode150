39238bf4397d161e9d4a5970536e9f17e8531604
Vinnicius Castro <vinnicius.castro97@gmail.com>
2025-06-13 17:29:01 -0600

    name updates

--- Trees/572.py
+++ Trees/572.py
@@ -1,4 +1,5 @@
 """
+Easy
 Leetcode 572. Subtree of Another Tree
 
 Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
