b7c2e7b1c17400c2ff04d66884a7d34756435d8e
Vinnicius Castro <90991299+vinniciuscastro@users.noreply.github.com>
2025-06-20 15:30:45 -0600

    something

--- Trees/1 Easy/94.py
+++ Trees/1 Easy/94.py
@@ -3,3 +3,17 @@ Easy
 LeetCode 94. Binary Tree Inorder Traversal
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
 """
+
+# Recursive approach
+def inorderTraversal(root: 'TreeNode') -> list[int]:
+    """
+    Perform inorder traversal of a binary tree using recursion.
+    
+    :param root: Root node of the binary tree
+    :return: List of node values in inorder
+    """
+    if not root:
+        return []
+    
+    # Traverse left subtree, visit root, then traverse right subtree
+    return inorderTraversal(root.left) + [root.val] + inorderTraversal(root.right)
