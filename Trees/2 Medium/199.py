7c463edb5bd18f14be6c96c360d0d8130deb7b8f
Vinnicius Castro <vinnicius.castro97@gmail.com>
2025-06-25 13:16:48 -0600

    add

---
+++ Trees/2 Medium/199.py
@@ -0,0 +1,5 @@
+"""
+LeetCode 199. Binary Tree Right Side View
+Given the root of a binary tree, return the values of the nodes you can see ordered 
+from top to bottom when looking at the tree from the right side.
+"""
