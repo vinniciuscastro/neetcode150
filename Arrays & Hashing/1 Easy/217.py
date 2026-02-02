1424ab5150fe63615ca346162856b4f49c33bd94
Vinnicius Castro <vinnicius.castro97@gmail.com>
2026-02-01 22:15:01 -0500

    contains duplicate: starting

---
+++ Arrays & Hashing/1 Easy/217.py
@@ -0,0 +1,6 @@
+"""
+Leetcode Problem 217: Contains Duplicate
+Given an integer array nums, return true if any value appears at least twice in the array,
+and return false if every element is distinct.
+
+"""
