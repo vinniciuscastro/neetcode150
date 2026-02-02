7707ab41a2f8a398b8c8c23dd5a55bcc571a228e
Vinnicius Castro <vinnicius.castro97@gmail.com>
2026-02-01 22:40:39 -0500

    Solved

--- Arrays & Hashing/1 Easy/217.py
+++ Arrays & Hashing/1 Easy/217.py
@@ -4,3 +4,21 @@ Given an integer array nums, return true if any value appears at least twice in 
 and return false if every element is distinct.
 
 """
+# Using a hashmap to track seen numbers
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        # Create a hashmap to track duplicates
+        dupli_check = {}
+        # Loop through each number in the array
+        for n in nums:
+            # If the number is already in the hashmap, we found a duplicate
+            if n in dupli_check:
+                return True
+            # Otherwise, add the number to the hashmap
+            else:
+                dupli_check[n] = n
+
+        return False
+
+# Time complexity: O(n), where n is the length of the nums array
+# Space complexity: O(n) in the worst case if all elements are distinct
